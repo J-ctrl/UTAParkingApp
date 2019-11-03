a6a5551d588cfcd494fcfd2b1a03b081aeb0ab39
Kadjo Anoh <kadjoanoh@Benjamins-MacBook-Pro.local>
2019-11-03 13:35:30 -0600

    URL load in the app and interpret the javacript

--- app/src/main/java/com/example/myapplication2/EventsList.java
+++ app/src/main/java/com/example/myapplication2/EventsList.java
@@ -3,12 +3,31 @@ package com.example.myapplication2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class EventsList extends AppCompatActivity {
+
+    //Reference the webview
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events_list);
+
+        //hook the class with the layout
+        mWebView = (WebView) findViewById(R.id.activity_event_webview);
+
+
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        mWebView.loadUrl("https://www.facebook.com/");
+
+        //Make the url stay inside the app
+        mWebView.setWebViewClient(new WebViewClient());
     }
 }
